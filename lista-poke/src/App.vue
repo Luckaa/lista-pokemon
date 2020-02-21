2c1d2ec40988010b581a047f5defc16638885175
Luckaa <lucasilva137@gmail.com>
2020-02-21 18:45:21 -0400

    pagination completa
    paginação completa e funcionando perfeitamente, também alinhei tudo em um container pra ficar centralizado e bonito

--- lista-poke/src/App.vue
+++ lista-poke/src/App.vue
@@ -1,29 +1,84 @@
 
 <template>
-  <div id="app">
+  <div  class="container" id="app">
         
+    <div> 
+      <nav>
+        <ul class="pagination justify-content-center">
+    <li v-if="this.pokeapi.offset>0" @click="anteriorpagPoke" class="page-item">
+      <a class="page-link" href="#">Anterior</a>
+    </li>
+    <li @click="pag0" class="page-item"><a href="#" class="page-link">1</a></li>
+    <li @click="pag1" class="page-item"><a href="#" class="page-link">2</a></li>
+    <li @click="pag2" class="page-item"><a href="#" class="page-link">3</a></li>
+    <li @click="pag3" class="page-item"><a href="#" class="page-link">4</a></li>
+    <li @click="pag4" class="page-item"><a href="#" class="page-link">5</a></li>
+    <li @click="pag5" class="page-item"><a href="#" class="page-link">6</a></li>
+    <li @click="pag6" class="page-item"><a href="#" class="page-link">7</a></li>
+    <li @click="pag7" class="page-item"><a href="#" class="page-link">8</a></li>
+    <li @click="pag8" class="page-item"><a href="#" class="page-link">9</a></li>
+    <li @click="pag9" class="page-item"><a href="#" class="page-link">10</a></li>
+    <li @click="pag10" class="page-item"><a href="#" class="page-link">11</a></li>
+    <li @click="pag11" class="page-item"><a href="#" class="page-link">12</a></li>
+    <li @click="pag12" class="page-item"><a href="#" class="page-link">13</a></li>
+    <li @click="pag13" class="page-item"><a href="#" class="page-link">14</a></li>
+    <li @click="pag14" class="page-item"><a href="#" class="page-link">15</a></li>
+    <li @click="pag15" class="page-item"><a href="#" class="page-link">16</a></li>
+    <li @click="pag16" class="page-item"><a href="#" class="page-link">17</a></li>
 
-    <div class="">
-    <div  class="row mt-6"> 
-    <div class="row col">
-      <ReposTec v-for="pokemon in pokemon" :key="pokemon" :pokemon="pokemon"/>
-    </div>
-    </div>
+    <li  v-if="this.pokeapi.offset!==960" @click="proximapagPoke" class="page-item">
+      <a href="#" class="page-link">Proximo</a>
+    </li>
 
-
-
-    </div>
-    <div>
-      <button  class="btn btn-info" @click="proximaPag">testar codigo</button>
-      <button  class="btn btn-info" @click="testar">testar pokemon</button>
-      <button  class="btn btn-info" @click="proximapagPoke">testar pag</button>
-
-    </div>
+      </ul>
+    </nav>
   </div>
+  
+
+    <div  class="container"> 
+    <div class="row justify-content-center">
+      <ReposTec v-for="pokemon in pokemon" :key="pokemon.id" :pokemon="pokemon"/>
+    </div>
+    </div>
+
+
+  <div>
+    <nav>
+        <ul class="pagination justify-content-center">
+    <li v-if="this.pokeapi.offset>0" @click="anteriorpagPoke" class="page-item">
+      <a class="page-link" href="#">Anterior</a>
+    </li>
+    <li @click="pag0" class="page-item"><a href="#" class="page-link">1</a></li>
+    <li @click="pag1" class="page-item"><a href="#" class="page-link">2</a></li>
+    <li @click="pag2" class="page-item"><a href="#" class="page-link">3</a></li>
+    <li @click="pag3" class="page-item"><a href="#" class="page-link">4</a></li>
+    <li @click="pag4" class="page-item"><a href="#" class="page-link">5</a></li>
+    <li @click="pag5" class="page-item"><a href="#" class="page-link">6</a></li>
+    <li @click="pag6" class="page-item"><a href="#" class="page-link">7</a></li>
+    <li @click="pag7" class="page-item"><a href="#" class="page-link">8</a></li>
+    <li @click="pag8" class="page-item"><a href="#" class="page-link">9</a></li>
+    <li @click="pag9" class="page-item"><a href="#" class="page-link">10</a></li>
+    <li @click="pag10" class="page-item"><a href="#" class="page-link">11</a></li>
+    <li @click="pag11" class="page-item"><a href="#" class="page-link">12</a></li>
+    <li @click="pag12" class="page-item"><a href="#" class="page-link">13</a></li>
+    <li @click="pag13" class="page-item"><a href="#" class="page-link">14</a></li>
+    <li @click="pag14" class="page-item"><a href="#" class="page-link">15</a></li>
+    <li @click="pag15" class="page-item"><a href="#" class="page-link">16</a></li>
+    <li @click="pag16" class="page-item"><a href="#" class="page-link">17</a></li>
+    <li  v-if="this.pokeapi.offset!==960" @click="proximapagPoke" class="page-item">
+      <a href="#" class="page-link">Proximo</a>
+    </li>
+      </ul>
+    </nav>
+    
+  </div>
+  </div>
+
 </template>
 <script>
 
 import ReposTec from'./components/ReposTec.vue';
+
 
 
 
@@ -66,27 +121,137 @@ export default {
   },
   components:{
 
-    ReposTec
+    ReposTec,
     
   },
   methods:{
     getPoke(){
       const {offset,urlPoke} = this.pokeapi
-      axios.get(`${urlPoke}?offset=${offset}&limit=30`).then(({data})=> this.pokemon = data["results"])
-      },
+      axios.get(`${urlPoke}?offset=${offset}&limit=60`).then(({data})=> this.pokemon = data["results"])
+      this.testar()},
 
       proximapagPoke(){
-      var proximoPoke = this.pokeapi.offset + 30
+      var proximoPoke = this.pokeapi.offset + 60
       this.pokeapi.offset = proximoPoke
       this.getPoke()
-      console.log(proximoPoke)
-      console.log(this.pokeapi.offset)
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+      anteriorpagPoke(){
+      var proximoPoke = this.pokeapi.offset - 60
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
       },
 
 
   testar(){
     console.log(this.pokemon)
   },
+  pag0(){
+      var proximoPoke = this.pokeapi.offset = 0
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag1(){
+      var proximoPoke = this.pokeapi.offset = 60
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag2(){
+      var proximoPoke = this.pokeapi.offset = 120
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag3(){
+      var proximoPoke = this.pokeapi.offset = 180
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag4(){
+      var proximoPoke = this.pokeapi.offset = 240
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag5(){
+      var proximoPoke = this.pokeapi.offset = 300
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag6(){
+      var proximoPoke = this.pokeapi.offset = 360
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag7(){
+      var proximoPoke = this.pokeapi.offset = 420
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag8(){
+      var proximoPoke = this.pokeapi.offset = 480
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag9(){
+      var proximoPoke = this.pokeapi.offset = 540
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag10(){
+      var proximoPoke = this.pokeapi.offset = 600
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag11(){
+      var proximoPoke = this.pokeapi.offset = 660
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag12(){
+      var proximoPoke = this.pokeapi.offset = 720
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag13(){
+      var proximoPoke = this.pokeapi.offset = 780
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag14(){
+      var proximoPoke = this.pokeapi.offset = 840
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag15(){
+      var proximoPoke = this.pokeapi.offset = 900
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  pag16(){
+      var proximoPoke = this.pokeapi.offset = 960
+      this.pokeapi.offset = proximoPoke
+      this.getPoke()
+      console.log(proximoPoke, this.pokeapi.offset)
+      },
+  
+
+
 
 
     getUser(e){
--- lista-poke/src/components/ReposTec.vue
+++ lista-poke/src/components/ReposTec.vue
@@ -20,6 +20,7 @@ export default {
     
     props:["pokemon"]
     ,
+  
     
 }
 </script>
